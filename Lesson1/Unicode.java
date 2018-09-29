0f9691419fdb87caafc1b493157a6387271a6a29
Denis A. Volkov <dvolkov555@gmail.com>
2018-09-29 13:15:10 +0500

    убрана переменная char, приведение к char внесено сразу в sout.println

--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 public class Unicode {	
 	public static void main(String[] args) {
 		for (int i = 33; i <= 126; i++) {
-			char ch = (char) i;
-			System.out.print(ch);
+			System.out.print((char) i);
 		}
 	}
 }
