13e4e9851b93a48e691699dcad847cdfbb6ecfab
Denis A. Volkov <vda@bankermak.ru>
2018-10-01 09:49:08 +0500

    Jaeger реализован с помощью конструктора

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -8,6 +8,24 @@ public class Jaeger {
 	private int strength;
 	private int armor;
 
+	public Jaeger(String modelName,
+				  String mark,
+				  String origin,
+				  float height,
+				  float weight,
+				  int speed,
+				  int strenght,
+				  int armor) {
+		this.modelName = modelName;
+		this.mark = mark;
+		this.origin = origin;
+		this.height = height;
+		this.weight = weight;
+		this.speed = speed;
+		this.strength = strength;
+		this.armor = armor;
+	}
+
 	public void setModelName(String modelName) {
 		this.modelName = modelName;
 	}
--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -1,26 +1,40 @@
 public class JaegerTest {	
 	public static void main(String[] args) {
-		Jaeger robot1 = new Jaeger();
-		robot1.setModelName("Bracer Phoenix");
-		robot1.setMark("Mark-5");
-		robot1.setOrigin("USA");
-		robot1.setHeight(70.7f);
-		robot1.setWeight(2.1f);
-		robot1.setSpeed(3);
-		robot1.setStrength(8);
-		robot1.setArmor(9);
+		Jaeger robot1 = new Jaeger("Bracer Phoenix",
+					   				"Mark-5",
+								   	"USA",
+								   	70.7f,
+								   	2.1f,
+								   	3,
+								   	8,
+								   	9);
+//		robot1.setModelName("Bracer Phoenix");
+//		robot1.setMark("Mark-5");
+//		robot1.setOrigin("USA");
+//		robot1.setHeight(70.7f);
+//		robot1.setWeight(2.1f);
+//		robot1.setSpeed(3);
+//		robot1.setStrength(8);
+//		robot1.setArmor(9);
 
-		Jaeger robot2 = new Jaeger();
-		robot2.setModelName("Gipsy Danger");
-		robot2.setMark("Mark-3");
-		robot2.setOrigin("USA");
-		robot2.setHeight(79.25f);
-		robot2.setWeight(1.98f);
-		robot2.setSpeed(7);
-		robot2.setStrength(8);
-		robot2.setArmor(6);
+		Jaeger robot2 = new Jaeger("Gipsy Danger",
+									"Mark-3",
+									"USA",
+									79.25f,
+									1.98f,
+									7,
+									8,
+									6);
+//		robot2.setModelName("Gipsy Danger");
+//		robot2.setMark("Mark-3");
+//		robot2.setOrigin("USA");
+//		robot2.setHeight(79.25f);
+//		robot2.setWeight(1.98f);
+//		robot2.setSpeed(7);
+//		robot2.setStrength(8);
+//		robot2.setArmor(6);
 
-		
+
 		System.out.println(robot1.getModelName());
 		System.out.println(robot1.getMark());
 		System.out.println(robot1.getOrigin());
