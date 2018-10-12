b680c4a8c768f62f38172eff7120d40e72fff312
Denis A. Volkov <vda@bankermak.ru>
2018-10-12 08:05:52 +0500

    исправления по замечаниям

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,13 +3,13 @@ CREATE DATABASE "Jaegers";
 \connect Jaegers;
 
 CREATE TABLE IF NOT EXISTS "robots" (
-	id serial not null primary key,
-	modelName VARCHAR(255) unique not null,
-	mark varchar(255) not null,
-	height decimal not null,
-	weight decimal not null,
-	status varchar not null,
-	origin varchar not null,
-	launch varchar not null,
-	kaijuKill integer not null
+	id			SERIAL		PRIMARY KEY,
+	modelName	VARCHAR		UNIQUE NOT NULL,
+	mark 		VARCHAR		NOT NULL,
+	height		DECIMAL		NOT NULL,
+	weight		DECIMAL		NOT NULL,
+	status 		VARCHAR		NOT NULL,
+	origin 		VARCHAR		NOT NULL,
+	launch		VARCHAR		NOT NULL,
+	kaijuKill	INTEGER		NOT NULL
 );
